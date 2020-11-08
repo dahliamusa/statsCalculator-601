f6179451dafd6f2751a24eb880b3d5df1819d598
dahliamusa <dahliajmusa@gmail.com>
2020-11-07 23:16:49 -0500

    Feature: Check values for being valid numbers and not strings

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,9 +1,12 @@
 from Calculator.Calculator import Calculator
+from AdditionalModules.HelperFunctions import valid
 from Statistics.Mean import mean
 
 
 class Statistics(Calculator):
 
     def mean(self, data):
+        if valid(data) == 0:
+            raise Exception("Data is not numeric")
         self.result = mean(data)
         return self.result
