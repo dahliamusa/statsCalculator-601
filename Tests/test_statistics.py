2bd95c67f2ef650e40f3d337a6c93718e5529b48
dahliamusa <dahliajmusa@gmail.com>
2020-11-07 19:20:23 -0500

    Feature: Calculate mode of data set and test result

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -8,7 +8,7 @@ import statistics
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.testData = [813, 731, 560, 32, 361, 41, 461, 450, 317, 976, 6, 370, 99, 601, 45, 209, 994, 100, 49, 916]
+        self.testData = [813, 731, 560, 32, 361, 41, 461, 450, 317, 976, 6, 99, 99, 601, 45, 209, 994, 100, 49, 916]
         self.statistics = Statistics()
 
     def test_instantiate_calculator(self):
@@ -17,6 +17,7 @@ class MyTestCase(unittest.TestCase):
     def test_mean_calculator(self):
         mean = self.statistics.mean(self.testData)
         self.assertEqual(mean, statistics.mean(self.testData))
+
 
 
 if __name__ == '__main__':
