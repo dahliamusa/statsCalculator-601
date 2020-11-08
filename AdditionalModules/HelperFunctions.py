e1f824683da2d06139ace6b7d0759ec5742bd19b
dahliamusa <dahliajmusa@gmail.com>
2020-11-07 23:19:13 -0500

    Feature: Check values for being valid numbers and not strings

--- AdditionalModules/HelperFunctions.py
+++ AdditionalModules/HelperFunctions.py
@@ -1,6 +1,12 @@
 import statistics
 
 
+def valid(data):
+    for num in data:
+        if type(num) == str:
+            return 0
+    return 1
+
 def stats_zscore(data):
     mean_value = statistics.mean(data)
     stdev_value = statistics.stdev(data)
--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,6 +1,11 @@
 from Calculator.Calculator import Calculator
 from AdditionalModules.HelperFunctions import valid
 from Statistics.Mean import mean
+from Statistics.Median import median
+from Statistics.Mode import mode
+from Statistics.Variance import variance
+from Statistics.StandardDeviation import stdev
+from Statistics.ZScore import zscore
 
 
 class Statistics(Calculator):
@@ -9,4 +14,34 @@ class Statistics(Calculator):
         if valid(data) == 0:
             raise Exception("Data is not numeric")
         self.result = mean(data)
+        return self.result
+
+    def median(self, data):
+        if valid(data) == 0:
+            raise Exception("Data is not numeric")
+        self.result = median(data)
+        return self.result
+
+    def mode(self, data):
+        if valid(data) == 0:
+            raise Exception("Data is not numeric")
+        self.result = mode(data)
+        return self.result
+
+    def variance(self, data):
+        if valid(data) == 0:
+            raise Exception("Data is not numeric")
+        self.result = variance(data)
+        return self.result
+
+    def stdev(self, data):
+        if valid(data) == 0:
+            raise Exception("Data is not numeric")
+        self.result = stdev(data)
+        return self.result
+
+    def zscore(self, data):
+        if valid(data) == 0:
+            raise Exception("Data is not numeric")
+        self.result = zscore(data)
         return self.result
